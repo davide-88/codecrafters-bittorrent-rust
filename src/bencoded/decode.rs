ce5e3feede0c101b089f090402d43a6c99b75503
Davide Mantelli <davide.mantelli@docebo.com>
2024-11-15 12:30:39 +0100

    refacotr: decode functions

--- src/bencoded/decode.rs
+++ src/bencoded/decode.rs
@@ -1,36 +1,37 @@
 use std::str::FromStr;
 
 fn decode_integer(chars: &str) -> serde_json::Value {
-  let end = chars.find(|c| c == 'e')
+    let end = chars
+        .find(|c| c == 'e')
         .unwrap_or_else(|| panic!("Failed to find end of integer"));
-  let raw_int = &chars[1..end];
-  serde_json::Value::Number(
-    serde_json::Number::from_i128(
-      raw_int.parse().expect("Failed to parse integer")
-    ).expect("Failed to convert integer to i128")
-  )
+    let raw_int = &chars[1..end];
+    serde_json::Value::Number(
+        serde_json::Number::from_i128(raw_int.parse().expect("Failed to parse integer"))
+            .expect("Failed to convert integer to i128"),
+    )
 }
 
 fn decode_string(chars: &str) -> serde_json::Value {
-  let divider_index = chars.find(|char| char == ':')
-    .unwrap_or_else(|| panic!("Failed to find string length"));
-  let length: usize = chars[0..divider_index].parse().expect("Failed to parse string length");
-  serde_json::Value::String(
-    String::from_str(&chars[divider_index + 1..=divider_index+length])
-    .unwrap_or_else(|_| panic!("Failed to parse string"))
-  )
+    let divider_index = chars
+        .find(|char| char == ':')
+        .unwrap_or_else(|| panic!("Failed to find string length"));
+    let length: usize = chars[0..divider_index]
+        .parse()
+        .expect("Failed to parse string length");
+    serde_json::Value::String(
+        String::from_str(&chars[divider_index + 1..=divider_index + length])
+            .unwrap_or_else(|_| panic!("Failed to parse string")),
+    )
 }
 
 pub fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-  let mut chars = encoded_value.chars();
-  match chars.nth(0) {
-    Some('i') => {
-      decode_integer(encoded_value)
-    },
-    Some('0'..='9') => decode_string(encoded_value),
-    None => serde_json::Value::Null,
-    _ => panic!("Unhandled encoded value: {}", encoded_value)
-  }
+    let mut chars = encoded_value.chars();
+    match chars.nth(0) {
+        Some('i') => decode_integer(encoded_value),
+        Some('0'..='9') => decode_string(encoded_value),
+        None => serde_json::Value::Null,
+        _ => panic!("Unhandled encoded value: {}", encoded_value),
+    }
 }
 
 #[cfg(test)]
@@ -46,6 +47,9 @@ mod tests {
     #[test]
     fn parse_integer() {
         let result = decode_bencoded_value(&"i52e");
-        assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(52)));
+        assert_eq!(
+            result,
+            serde_json::Value::Number(serde_json::Number::from(52))
+        );
     }
 }
