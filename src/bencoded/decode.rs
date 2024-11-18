cf4af55a75633c99a4041888f58b7a840f1f2d07
Davide Mantelli <davide.mantelli@docebo.com>
2024-11-18 18:13:40 +0100

    feat: decode list

--- src/bencoded/decode.rs
+++ src/bencoded/decode.rs
@@ -1,37 +1,81 @@
 use std::str::FromStr;
 
-fn decode_integer(chars: &str) -> serde_json::Value {
-    let end = chars
-        .find(|c| c == 'e')
-        .unwrap_or_else(|| panic!("Failed to find end of integer"));
-    let raw_int = &chars[1..end];
-    serde_json::Value::Number(
-        serde_json::Number::from_i128(raw_int.parse().expect("Failed to parse integer"))
-            .expect("Failed to convert integer to i128"),
-    )
+fn decode_integer(chars: &str) -> (serde_json::Value, Option<&str>) {
+    chars
+        .split_once('i')
+        .and_then(|(_, interger_with_remainder)| interger_with_remainder.split_once('e'))
+        .and_then(|(digits, remainder)| {
+            digits
+                .parse::<i128>()
+                .map(|number| {
+                    serde_json::Value::Number(serde_json::Number::from_i128(number).unwrap_or_else(
+                        || panic!("{}", format!("Failed to parse integer {digits}")),
+                    ))
+                })
+                .ok()
+                .map(|value| (value, Some(remainder)))
+        })
+        .unwrap_or_else(|| panic!("{}", format!("Failed to find start of integer in {chars}")))
 }
 
-fn decode_string(chars: &str) -> serde_json::Value {
+fn decode_string(chars: &str) -> (serde_json::Value, Option<&str>) {
     let divider_index = chars
         .find(|char| char == ':')
-        .unwrap_or_else(|| panic!("Failed to find string length"));
+        .unwrap_or_else(|| panic!("{}", format!("Failed to find string length of {chars}")));
     let length: usize = chars[0..divider_index]
         .parse()
-        .expect("Failed to parse string length");
-    serde_json::Value::String(
-        String::from_str(&chars[divider_index + 1..=divider_index + length])
-            .unwrap_or_else(|_| panic!("Failed to parse string")),
+        .unwrap_or_else(|_| panic!("{}", format!("Failed to find string length of {chars}")));
+    let end = divider_index + length + 1;
+    (
+        serde_json::Value::String(
+            String::from_str(&chars[divider_index + 1..=divider_index + length]).unwrap_or_else(
+                |_| {
+                    panic!(
+                        "{}",
+                        format!("Failed to parse string {}", &chars[divider_index + 1..end])
+                    )
+                },
+            ),
+        ),
+        chars.get(end..),
     )
 }
 
-pub fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_list(chars: &str) -> (serde_json::Value, Option<&str>) {
+    let mut list = Vec::new();
+    let mut remainder: Option<&str> = chars
+        .split_once('l')
+        .and_then(|(_, next_chars)| Some(next_chars))
+        .filter(|next_chars| next_chars.len() > 0);
+    while let Some(next_chars) = remainder.filter(|rem| !rem.starts_with('e')) {
+        let (value, rest) = decode_bencoded_value_and_remainder(next_chars);
+        remainder = rest;
+        list.push(value);
+    }
+    (
+        serde_json::Value::Array(list),
+        remainder
+            .and_then(|rem| rem.split_once('e'))
+            .and_then(|(_, rem)| Some(rem))
+            .filter(|rem| rem.len() > 0),
+    )
+}
+
+fn decode_bencoded_value_and_remainder(encoded_value: &str) -> (serde_json::Value, Option<&str>) {
     let mut chars = encoded_value.chars();
     match chars.nth(0) {
         Some('i') => decode_integer(encoded_value),
         Some('0'..='9') => decode_string(encoded_value),
-        None => serde_json::Value::Null,
+        Some('l') => decode_list(encoded_value),
+        None => (serde_json::Value::Null, None),
         _ => panic!("Unhandled encoded value: {}", encoded_value),
     }
+}
+
+pub fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    let decoded = decode_bencoded_value_and_remainder(encoded_value);
+    println!("decoded: {:?}", decoded);
+    decoded.0
 }
 
 #[cfg(test)]
@@ -50,6 +94,18 @@ mod tests {
         assert_eq!(
             result,
             serde_json::Value::Number(serde_json::Number::from(52))
+        );
+    }
+
+    #[test]
+    fn parse_list() {
+        let result = decode_bencoded_value(&"l5:helloi52ee");
+        assert_eq!(
+            result,
+            serde_json::Value::Array(vec![
+                serde_json::Value::String(String::from("hello")),
+                serde_json::Value::Number(serde_json::Number::from(52))
+            ])
         );
     }
 }
